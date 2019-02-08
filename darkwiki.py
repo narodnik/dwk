1bad9178bcb39901c38de2000d66611add375f2c
gdr <foo@foo.bar>
2019-02-08 17:23:49 +0100

    added most commands for VFS and diff algorithm

--- darkwiki.py
+++ darkwiki.py
@@ -20,6 +20,18 @@ def find_root_path():
             return path
         path = move_up(path)
 
+def touch_file(filename):
+    open(filename, 'wb').write(b'')
+
+def remove_if(vector, predicate):
+    return [x for x in vector if not predicate(x)]
+
+def filter_one(vector, predicate):
+    match = [x for x in vector if predicate(x)]
+    if not match:
+        return None
+    return match[0]
+
 class DataType(Enum):
     BLOB   = 1
     TREE   = 2
@@ -37,18 +49,23 @@ class DiskDatabase:
     def _objects_path(self):
         return os.path.join(self._dot_path, 'objects')
     @property
-    def _refs_path(self):
-        return os.path.join(self._dot_path, 'refs')
+    def _HEAD_path(self):
+        return os.path.join(self._dot_path, 'HEAD')
     @property
     def _index_filename(self):
         return os.path.join(self._dot_path, 'index')
+
+    def _ref_path(self, ref):
+        return os.path.join(self._dot_path, ref)
 
     def _open(self, ident, flag):
         return open(os.path.join(self._objects_path, ident), flag + 'b')
 
     def initialize(self):
         os.mkdir(self._objects_path)
-        os.mkdir(self._refs_path)
+        self._write_HEAD('refs/heads/master')
+        os.makedirs(self._ref_path('refs/heads/'))
+        touch_file(self._index_filename)
 
     def _add_data(self, data, data_type):
         ident = hashlib.sha256(data).hexdigest()
@@ -60,6 +77,14 @@ class DiskDatabase:
 
     def add_blob(self, data):
         return self._add_data(data, DataType.BLOB)
+
+    def add_file(self, filename):
+        data = open(filename, 'rb').read()
+        ident = self.add_blob(data)
+        # filename relative to root
+        filename = os.path.relpath(filename, self._root_path)
+
+        self.update_index('644', ident, filename)
 
     def list(self):
         return os.listdir(self._objects_path)
@@ -102,8 +127,22 @@ class DiskDatabase:
         return self.fetch(ident)[0]
 
     def update_index(self, mode, ident, filename):
-        with open(self._index_filename, 'a') as file_handle:
-            file_handle.write('%s %s %s\n' % (mode, ident, filename))
+        index = self.read_index()
+        index = remove_if(index, lambda row: row[2] == filename)
+        index.append((mode, ident, filename))
+
+        self._write_to_index(index)
+
+    def remove_from_index(self, filename):
+        index = self.read_index()
+        index = remove_if(index, lambda row: row[2] == filename)
+
+        self._write_to_index(index)
+
+    def _write_to_index(self, index):
+        with open(self._index_filename, 'w') as file_handle:
+            for mode, ident, filename in index:
+                file_handle.write('%s %s %s\n' % (mode, ident, filename))
 
     def clear_index(self):
         open(self._index_filename, 'w').truncate(0)
@@ -116,14 +155,10 @@ class DiskDatabase:
                 assert line[-1] == '\n'
                 line = line[:-1]
                 mode, ident, filename = line.split(' ')
-                object_type = self.object_type(ident)
-                index.append((mode, object_type, ident, filename))
+                index.append((mode, ident, filename))
         return index
 
     def _create_subtrees(self, index):
-        index = [(mode, ident, filename) for mode, object_type, ident, filename
-                 in index if object_type == DataType.BLOB]
-
         root = darkwiki.build_tree(index)
 
         for directory in darkwiki.walk_tree(root):
@@ -135,10 +170,10 @@ class DiskDatabase:
                 description += '%s BLOB %s %s\n' % (mode, ident, filename)
             for subdir in directory.subdirs:
                 description += '755 TREE %s %s\n' % (subdir.ident, subdir.name)
-            print('==============')
-            print(directory.full_path)
-            print(description)
-            print('==============')
+            #print('==============')
+            #print(directory.full_path)
+            #print(description)
+            #print('==============')
             # Write this index
             data = description.encode()
             ident = self._add_data(data, DataType.TREE)
@@ -150,23 +185,30 @@ class DiskDatabase:
     def write_tree(self):
         # Read index and clear
         index = self.read_index()
-        ##self.clear_index()
         # Create subtrees
         root_tree_ident = self._create_subtrees(index)
         return root_tree_ident
-        #ident = self._add_data(data, DataType.TREE)
-        #return ident
 
-    def add_file(self, filename):
-        data = open(filename, 'rb').read()
-        ident = self.add_blob(data)
-        # filename relative to root
-        filename = os.path.relpath(filename, self._root_path)
-        self.update_index('644', ident, filename)
+    def _write_HEAD(self, ref):
+        with open(self._HEAD_path, 'w') as file_handle:
+            file_handle.write('ref: %s' % ref)
 
-    def _last_commit_ident(self):
+    def _get_ref_commit_ident(self, reference):
+        path = self._ref_path(reference)
+        try:
+            return open(path).read()
+        except FileNotFoundError:
+            return None
+
+    def _get_current_ref(self):
+        HEAD_data = open(self._HEAD_path).read()
+        assert HEAD_data.startswith('ref: ')
+        return HEAD_data[len('ref: '):]
+
+    def last_commit_ident(self):
+        reference = self._get_current_ref()
         # lookup from ref in HEAD
-        return 'hello'
+        return self._get_ref_commit_ident(reference)
 
     def commit(self):
         # write_tree()
@@ -181,14 +223,110 @@ class DiskDatabase:
             'timestamp': unix_time,
             'utc_offset': utc_offset,
             # Previous commit
-            'previous_commit': self._last_commit_ident()
+            'previous_commit': self.last_commit_ident()
             # later we will add hash of pubkey for ID
         }
         data = json.dumps(commit).encode()
         commit_ident = self._add_data(data, DataType.COMMIT)
         print('commit:', commit_ident)
+
+        self._write_to_ref(commit_ident)
+
+    def _write_to_ref(self, commit_ident):
         # lookup current ref in HEAD
+        reference = self._get_current_ref()
         # update refs/<...>
+        path = self._ref_path(reference)
+        open(path, 'w').write(commit_ident)
+
+class Interface:
+
+    def __init__(self, db):
+        self._db = db
+
+    def fetch_commits(self):
+        results = []
+        current_commit_ident = self._db.last_commit_ident()
+        while current_commit_ident is not None:
+            object_type, commit = self._db.fetch(current_commit_ident)
+            commit['ident'] = current_commit_ident
+            assert object_type == DataType.COMMIT
+            results.append(commit)
+            current_commit_ident = commit['previous_commit']
+        return results
+
+    def _read_tree(self, tree_ident, tree_name=None):
+        object_type, tree_contents = self._db.fetch(tree_ident)
+        assert object_type == DataType.TREE
+
+        tree = darkwiki.DirectoryTree(tree_name)
+
+        for mode, object_type, ident, filename in tree_contents:
+            if object_type == DataType.BLOB:
+                tree.add_file(mode, ident, filename)
+            elif object_type == DataType.TREE:
+                subtree = self._read_tree(ident, filename)
+                tree.add_subdir(subtree)
+
+        return tree
+
+    def diff_cached(self, commit_ident):
+        # diff current index state and files with commit ident
+        if commit_ident is None:
+            commit_ident = self._db.last_commit_ident()
+
+            if commit_ident is None:
+                return None
+
+        assert commit_ident is not None
+
+        # Read root tree of commit
+        object_type, commit = self._db.fetch(commit_ident)
+        assert object_type == DataType.COMMIT
+
+        tree_root_ident = commit['tree']
+        tree = self._read_tree(tree_root_ident)
+
+        # Read index
+        index = self._db.read_index()
+
+        results = []
+        # For each file in index
+        for mode, ident, filename in index:
+            # Lookup in tree
+            filespec = lookup_file_in_tree(tree, filename)
+            if filespec is None:
+                diff = 'foo'
+                continue
+
+            # If ident doesn't match, perform diff
+            if filespec[1] == ident:
+                continue
+
+            print(filename, 'doesnt match')
+            diff = 'foo - foo'
+
+            # Add to results
+            results.append(diff)
+
+        # Return results
+        return results
+
+def lookup_file_in_tree(tree_root, filename):
+    file_path = os.path.dirname(filename)
+    file_base = os.path.basename(filename)
+
+    match_dir = filter_one(darkwiki.walk_tree(tree_root),
+                           lambda directory: directory.full_path == file_path)
+    if match_dir is None:
+        return None
+
+    match_file = filter_one(match_dir.files,
+                            lambda file: file[2] == file_base)
+    if match_file is None:
+        return None
+
+    return match_file
 
 def main():
     parser = argparse.ArgumentParser(prog='darkwiki')
@@ -205,9 +343,14 @@ def main():
     parser_add_object.set_defaults(func=add_object)
 
     # add
-    parser_add_object = subparsers.add_parser('add')
-    parser_add_object.add_argument('filename')
-    parser_add_object.set_defaults(func=simple_add)
+    parser_add = subparsers.add_parser('add')
+    parser_add.add_argument('filename')
+    parser_add.set_defaults(func=simple_add)
+
+    # rm
+    parser_rm = subparsers.add_parser('rm')
+    parser_rm.add_argument('filename')
+    parser_rm.set_defaults(func=simple_rm)
 
     # list
     parser_list = subparsers.add_parser('list')
@@ -241,6 +384,15 @@ def main():
     parser_commit = subparsers.add_parser('commit')
     parser_commit.set_defaults(func=commit)
 
+    # log
+    parser_log = subparsers.add_parser('log')
+    parser_log.set_defaults(func=log)
+
+    # diff
+    parser_diff = subparsers.add_parser('diff')
+    parser_diff.add_argument('commit_ident', nargs='?', default=None)
+    parser_diff.set_defaults(func=diff)
+
     args = parser.parse_args()
 
     if args.func is None:
@@ -268,6 +420,11 @@ def simple_add(parser):
     db.add_file(parser.filename)
     return 0
 
+def simple_rm(parser):
+    db = DiskDatabase()
+    db.remove_from_index(parser.filename)
+    return 0
+
 def list_objects(parser):
     db = DiskDatabase()
 
@@ -292,8 +449,8 @@ def update_index(parser):
 def read_index(parser):
     db = DiskDatabase()
     current_index = db.read_index()
-    for mode, object_type, ident, filename in current_index:
-        print(mode, object_type.name, ident, filename)
+    for mode, ident, filename in current_index:
+        print(mode, ident, filename)
     return 0
 
 def write_tree(parser):
@@ -337,6 +494,27 @@ def show_type(parser):
 def commit(parser):
     db = DiskDatabase()
     db.commit()
+
+def log(parser):
+    db = DiskDatabase()
+    interface = Interface(db)
+    log = interface.fetch_commits()
+    previous = None
+    for commit in log:
+        if previous is not None:
+            assert commit['ident'] == previous
+        print(commit['ident'])
+        print(commit['timestamp'], '+%s' % commit['utc_offset'])
+        previous = commit['previous_commit']
+        print()
+    #print(json.dumps(log, indent=2))
+
+def diff(parser):
+    db = DiskDatabase()
+    interface = Interface(db)
+    diff_result = interface.diff_cached(parser.commit_ident)
+    for row in diff_result:
+        print(row)
 
 if __name__ == '__main__':
     sys.exit(main())
---
+++ darkwiki/diff.py
@@ -0,0 +1,140 @@
+import diff_match_patch as dmp_module
+
+dmp = dmp_module.diff_match_patch()
+
+def changes_table(text, diffs):
+    changes = list(text)
+    index = 0
+    for change, change_text in diffs:
+        if change == 1:
+            continue
+        for i, letter in enumerate(change_text):
+            changes[index + i] = [change, letter, []]
+        index += len(change_text)
+    assert index == len(text)
+    return changes
+
+def merge_changes_tables(changes_2, changes_3):
+    changes_merged = changes_2[:]
+    for i, (change, letter, _) in enumerate(changes_3):
+        if change == -1:
+            changes_merged[i] = (-1, letter, [])
+    return changes_merged
+
+def append_additions(changes, diffs):
+    index = 0
+    for change, change_text in diffs:
+        if change != 1:
+            index += len(change_text)
+            continue
+        changes[index][2].append(change_text)
+
+def emerge_diff_from_changes(changes):
+    diff_merged = []
+    previous_change = None
+    previous_sentence = None
+    for change, letter, add_list in changes:
+        if add_list:
+            # Add current stuff
+            # ... only when not at the very start
+            if previous_change is not None:
+                assert previous_sentence is not None
+                diff_merged.append((previous_change, previous_sentence))
+            # Then additions
+            additions = "".join(addition for addition in add_list)
+            diff_merged.append((1, additions))
+            # Reset
+            previous_sentence = None
+            previous_change = None
+
+        # Continue along
+        if change == previous_change:
+            previous_sentence += letter
+            continue
+
+        # Push current sentence
+        if previous_change is not None:
+            diff_merged.append((previous_change, previous_sentence))
+        # Reset
+        previous_sentence = letter
+        previous_change = change
+
+    # Add remaining stuff
+    diff_merged.append((previous_change, previous_sentence))
+    return diff_merged
+
+def print_diff(diffs):
+    for change, diff in diffs:
+        if change == 0:
+            text = diff
+        elif change == -1:
+            text = colored(diff, 'red')
+        elif change == 1:
+            text = colored(diff, 'green')
+        print(text, end='')
+
+def three_way_merge(base_text_1, text_2, text_3):
+    diffs_2 = dmp.diff_main(base_text_1, text_2)
+    diffs_3 = dmp.diff_main(base_text_1, text_3)
+    dmp.diff_cleanupSemantic(diffs_2)
+    dmp.diff_cleanupSemantic(diffs_3)
+
+    changes_2 = changes_table(base_text_1, diffs_2)
+    changes_3 = changes_table(base_text_1, diffs_3)
+
+    changes_merged = merge_changes_tables(changes_2, changes_3)
+    # Add an extra empty to the change table at the end
+    # for additions added to the end of the text
+    changes_merged.append((0, '', []))
+
+    append_additions(changes_merged, diffs_2)
+    append_additions(changes_merged, diffs_3)
+
+    return emerge_diff_from_changes(changes_merged)
+
+def difference(base_text_1, text_2):
+    diffs = dmp.diff_main(base_text_1, text_2)
+    dmp.diff_cleanupSemantic(diffs)
+    return diffs
+
+if __name__ == '__main__':
+    from termcolor import colored
+
+    text_1 = """
+I am the very model of a modern Major-General,
+I've information vegetable, animal, and mineral,
+I know the kings of England, and I quote the fights historical,
+From Marathon to Waterloo, in order categorical.
+"""
+
+    text_2 = """
+I am the very model of a modern Major-General,
+I've had information vegetable and mineral,
+I know the kings of England, and I quote the fights historical,
+From Marathon to Waterloo, in order categorical.
+"""
+
+    text_3 = """
+I am the very model of a modern Major-General,
+I've information vegetable, and mineral,
+You know queens of England, and I quote the fights historical,
+From Marathon to Waterloo, in order categorical.
+"""
+
+    print('==== Diff(1, 2) ====')
+    diffs = difference(text_1, text_2)
+    print_diff(diffs)
+    print('====================')
+    print()
+
+    print('==== Diff(1, 3) ====')
+    diffs = difference(text_1, text_3)
+    print_diff(diffs)
+    print('====================')
+    print()
+
+    print('=== Diff(1, 2, 3) ==')
+    diffs = three_way_merge(text_1, text_2, text_3)
+    print_diff(diffs)
+    print('====================')
+
